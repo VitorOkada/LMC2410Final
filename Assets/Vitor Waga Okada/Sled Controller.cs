e72451ebb33ca6a004430aaca0e7d408301f4ec0
Vitor Waga Okada <vitorw.okada@gmail.com>
2025-04-01 15:48:26 -0400

    One jump sled mechanic

--- Assets/Vitor Waga Okada/Sled Controller.cs
+++ Assets/Vitor Waga Okada/Sled Controller.cs
@@ -9,7 +9,9 @@ public class NewBehaviourScript : MonoBehaviour
 
 {
     [SerializeField] float speed;
+    [SerializeField] float jump;
     private float Move;
+    bool grounded;
     private Rigidbody2D sled;
     // Start is called before the first frame update
     void Start()
@@ -22,5 +24,27 @@ public class NewBehaviourScript : MonoBehaviour
     {
         Move = Input.GetAxis("Horizontal");
         sled.velocity = new Vector2(Move * speed, sled.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+            Debug.Log("JUMP PRESSED");
+            sled.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) {
+            Vector3 normal = other.GetContact(0).normal;
+            if (normal == Vector3.up) {
+                grounded = true;
+            }
+        }  
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) {
+            grounded = false;
+        }
     }
 }
